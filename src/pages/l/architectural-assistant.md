8841ca17ecb927c61f5d2ac6d587418d0650aff7
Michele Memoli <michele@100shapes.com>
2018-05-10 22:37:52 +0100

    Added an example landing page

--- src/pages/l/architectural-assistant.md
+++ src/pages/l/architectural-assistant.md
@@ -1,11 +1,8 @@
 ---
 templateKey: landing
-title: Wonderful project 1
+title: Architectural Assistant
 date: 2016-12-17T15:04:10.000Z
 description: The Coffee Taster’s Flavor Wheel, the official resource used by coffee tasters, has been revised for the first time this year.
-tags:
-  - flavor
-  - tasting
 ---
 
 ![flavor wheel](/img/flavor_wheel.jpg)
--- src/templates/landing.js
+++ src/templates/landing.js
@@ -4,6 +4,8 @@ import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import Content, { HTMLContent } from '../components/Content'
+import { Container } from 'reactstrap'
+import Banner from '../components/Banner'
 
 export const ProjectTemplate = ({
   content,
@@ -16,32 +18,17 @@ export const ProjectTemplate = ({
   const PostContent = contentComponent || Content
 
   return (
-    <section className="section">
-      {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
-    </section>
+    <article>
+      <Helmet>
+        <title children={title} />
+        <meta name="description" content={description} />
+      </Helmet>
+      <Banner heading={title} />
+      <Container>
+        <p>{description}</p>
+        <PostContent content={content} />
+      </Container>
+    </article>
   )
 }
 
@@ -50,7 +37,6 @@ ProjectTemplate.propTypes = {
   contentComponent: PropTypes.func,
   description: PropTypes.string,
   title: PropTypes.string,
-  helmet: PropTypes.instanceOf(Helmet),
 }
 
 const Project = ({ data }) => {
@@ -61,7 +47,6 @@ const Project = ({ data }) => {
       content={post.html}
       contentComponent={HTMLContent}
       description={post.frontmatter.description}
-      helmet={<Helmet title={`${post.frontmatter.title} | Project`} />}
       tags={post.frontmatter.tags}
       title={post.frontmatter.title}
     />
